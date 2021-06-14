92cbb8feeede3ca9df7c1bd1578c5ce971836784
Kyoungho Min <khms4802@gmail.com>
2021-06-14 12:21:33 +0900

    June 14th

---
+++ Level_1/2588.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a*((b%100)%10));
+	printf("%d\n", a*((b%100)/10));
+	printf("%d\n", a*(b/100));
+	printf("%d\n", (a*((b%100)%10))*1 + (a*((b%100)/10))*10 + a*(b/100)*100);
+}}
