b1ae8891907d3c9ae838b0566745eb75cc93bce4
Kyoungho Min <khms4802@gmail.com>
2021-06-16 23:06:58 +0900

    June 16th

---
+++ Level_2/14681.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+	int x,y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+
+	if (x > 0){
+		if (y > 0)
+			printf("1\n");
+		else if (y < 0)
+			printf("4\n");
+
+	} else if (x < 0) {
+		if (y > 0)
+			printf("2\n");
+		else if (y < 0)
+			printf("3\n");
+
+	}
+	return 0;
+}
