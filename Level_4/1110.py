0b0ef40a812ee269578ecd0dd6d02211878920bd
Kyoungho Min <khms4802@gmail.com>
2021-07-23 15:39:30 +0900

    June 23rd

--- Level_4/1110.py
+++ Level_4/1110.py
@@ -1,15 +1,14 @@
-counter, tmp, s = 0, 0, 0
-num = int(input())
-while num != s:
-    if num < 10:
-        num *= 2
-        counter += 1
-        s = num
-    elif num >= 10:
-        tmp = num % 10
-        num = tmp + num // 10
-        num = num + tmp
-        counter += 1
-        s = num
+start = int(input())
+tmp, counter = start, 0
 
+while True:
+    if tmp < 10:
+        tmp *= 11
+        counter += 1
+    elif tmp >= 10:
+        tmp_1 = tmp % 10
+        tmp = tmp // 10 + tmp_1
+        tmp = tmp_1 * 10 + tmp % 10
+        counter += 1
+    if tmp == start : break
 print(counter)
