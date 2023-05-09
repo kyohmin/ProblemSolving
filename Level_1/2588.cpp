b530660f349b182a86051d5347df3de7203a6f8c
민경호 <khms_macbookpro@KHMSMacBookPro.local>
2023-05-09 17:37:54 +0900

    Finised Level_1

---
+++ Level_1/2588.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void digitCalc(int num_1, int num_2, int digit);
+
+using namespace std;
+int main(){
+    int threeDigit1, threeDigit2;
+    cin >> threeDigit1 >> threeDigit2;
+    digitCalc(threeDigit1,threeDigit2,1);
+    digitCalc(threeDigit1,threeDigit2,10);
+    digitCalc(threeDigit1,threeDigit2,100);
+    cout << threeDigit1*threeDigit2;
+
+    return 0;
+}
+
+void digitCalc(int num_1, int num_2, int digit){
+    int tmp_digit = digit * 10;
+    cout << num_1*((num_2 % tmp_digit)/digit) << endl;
+}
