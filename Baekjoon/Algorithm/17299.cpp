a142079891662e762bc016f27dfe409492d8fbbf
Kyoungho Min <kyohmin@gmail.com>
2023-05-25 22:49:33 +0900

    Daily problem added

---
+++ Baekjoon/Algorithm/17299.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+
+int li[1000000];
+int cnt[1000000];
+int result[1000000];
+
+using namespace std;
+int main(){
+    stack<int> idx;
+
+    int rep;
+    cin >> rep;
+    for(int i = 0; i < rep; i++){
+        cin >> li[i];
+        cnt[li[i]] += 1;
+    }
+
+    for(int i = 0; i < rep; i++){
+        while(!idx.empty() && cnt[li[idx.top()]] < cnt[li[i]]){
+            result[idx.top()] = li[i];
+            idx.pop();
+        }
+
+        idx.push(i);
+    }
+
+    while(!idx.empty()){
+       result[idx.top()] = -1;
+       idx.pop();
+    }
+
+    for(int i= 0; i < rep; i++){
+        cout << result[i] << ' ';
+    }
+}
