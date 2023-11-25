72a32dbe389a119845c34967d1f2eb63c4588d8f
Kyoungho MIn <khms4802@gmail.com>
2023-11-25 21:26:05 +0800

    Solved 121.Best Time to Buy and Sell Stock. 2/75

--- PythonPS/Leetcode/Leetcode75/1431.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        maxVal = max(candies)
-        result = []
-        for i in candies:
-            result.append(i+extraCandies >= maxVal)
-
-        return result
