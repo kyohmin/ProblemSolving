7c25c17a0b3d3999de7679a02f9ad1e046811d3f
Kyoungho MIn <khms4802@gmail.com>
2023-11-25 22:06:13 +0800

    Solved BOJ 28278 S4

---
+++ PythonPS/BOJ/28278.py
@@ -0,0 +1,48 @@
+"""
+Problem Name: Stack 2
+Date        : Nov-25,2023
+Link        : https://www.acmicpc.net/problem/28278
+"""
+
+# Import
+import sys
+
+# Solution
+stack = []
+repeat= int(sys.stdin.readline())
+for i in range(repeat):
+    # Command Input
+    cmd = sys.stdin.readline().rstrip()
+
+    # Push to the stack -> None
+    if len(cmd) > 1:
+        val = int(cmd[2:])
+        stack.append(val)
+
+    # Pop stack -> Int
+    elif cmd == '2':
+        if len(stack) == 0: print(-1)
+        else:
+            print(stack.pop())
+
+    # Size of Stack -> Int
+    elif cmd == '3':
+        print(len(stack))
+
+    # Is empty -> Int (bool)
+    elif cmd == '4':
+        if len(stack) == 0: print(1)
+        else: print(0)
+
+    # Top value in Stack
+    elif cmd == '5':
+        if len(stack) == 0: print(-1)
+        else:
+            print(stack[-1])
+
+"""
+Note:
+    PS for implementing stack.
+    Used 'int(sys.stdin.readline())' for faster input speed.
+
+"""
