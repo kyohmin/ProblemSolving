813a5692765f32b34342ea61c1d93c3aae41a906
Kyoungho MIn <khms4802@gmail.com>
2023-11-26 10:39:10 +0800

    Solved 3.Contains Duplicate 3/75

---
+++ PythonPS/Leetcode/Blind75/3.py
@@ -0,0 +1,19 @@
+"""
+Name: Contains Duplicate
+Date: 2023-11-26
+"""
+
+# Solution 1 - Using Set
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        record = set()
+        for i in nums:
+            if i in record: return True
+            record.add(i)
+        return False
+    
+
+"""
+Note:
+    To find if the list contains any duplicate, set can be used.
+"""
